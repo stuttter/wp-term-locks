54c3b119755756b982c4bbf6a55e90b3c7b0309f
John James Jacoby <johnjamesjacoby@me.com>
2016-01-07 18:41:07 -0600

    Update readme's and loading process.

--- wp-term-locks.php
+++ wp-term-locks.php
@@ -14,7 +14,7 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Instantiate the main WordPress Term Locks class
+ * Include the required files & dependencies
  *
  * @since 0.1.0
  */
@@ -30,13 +30,11 @@ function _wp_term_locks() {
 add_action( 'plugins_loaded', '_wp_term_locks' );
 
 /**
- * Instantiate the main WordPress Term Locks class
+ * Instantiate the main class
  *
  * @since 0.2.0
  */
 function _wp_term_locks_init() {
-
-	// Instantiate the main class
 	new WP_Term_Locks( __FILE__ );
 }
 add_action( 'init', '_wp_term_locks_init', 88 );

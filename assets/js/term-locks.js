d92d4ce9e5040f43df535b7dce279df8100d1aae
John James Jacoby <johnjamesjacoby@me.com>
2016-01-08 00:19:50 -0600

    Enqueue CSS and simplify JS.

--- assets/js/term-locks.js
+++ assets/js/term-locks.js
@@ -1,16 +1,16 @@
-jQuery( document ).ready( function() {
+jQuery( document ).ready( function( $ ) {
     'use strict';
 
-    if ( typeof jQuery.wp === 'object' && typeof jQuery.wp.wpLockPicker === 'function' ) {
-        jQuery( '#term-lock' ).wpLockPicker();
+    if ( typeof $.wp === 'object' && typeof $.wp.wpLockPicker === 'function' ) {
+        $( '#term-lock' ).wpLockPicker();
     } else {
-        jQuery( '#lockpicker' ).farbtastic( '#term-lock' );
+        $( '#lockpicker' ).farbtastic( '#term-lock' );
     }
 
-    jQuery( '.editinline' ).on( 'click', function() {
-        var tag_id = jQuery( this ).parents( 'tr' ).attr( 'id' ),
-			lock  = jQuery( 'td.lock i', '#' + tag_id ).attr( 'data-lock' );
+    $( '.editinline' ).on( 'click', function() {
+        var tag_id = $( this ).parents( 'tr' ).attr( 'id' ),
+			lock   = $( 'td.lock i', '#' + tag_id ).data( 'lock' );
 
-        jQuery( ':input[name="term-lock"]', '.inline-edit-row' ).val( lock );
+        $( ':input[name="term-lock"]', '.inline-edit-row' ).val( lock );
     } );
 } );
